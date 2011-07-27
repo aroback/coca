9fb7b101a8d36543c403ade3ee18ee38a481388e
arobck <andrew.roback@gmail.com>
2011-07-26 22:48:03 -0500

    mechanize works, but frames are still a problem

--- coca.rb
+++ coca.rb
@@ -13,13 +13,16 @@ page = agent.submit(entry_form)
 
 puts "Entered COCA Website"
 
+page = agent.get('login1.asp')
+
 #log in
 login_form = page.form("myform")
-login_form.email = EMAIL
-login_form.password = PASSWORD
+  login_form.email = EMAIL
+  login_form.password = PASSWORD
 page = agent.submit(login_form)
-
 puts "Logged in"
+
+page = agent.get('http://www.americancorpus.org/')
 
 pp page
 
