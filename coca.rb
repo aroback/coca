cb7b1b417dde22b85b5195092249c7bf5fc17a52
arobck <andrew.roback@gmail.com>
2011-07-26 19:53:51 -0500

    didn't work, but saving anyways

--- coca.rb
+++ coca.rb
@@ -1,7 +1,26 @@
-require 'rubygems'
-require 'mechanize'
+require "rubygems"
+require "mechanize"
+require "config/constants"
 
 agent = Mechanize.new
+
+#open COCA page
 page = agent.get('http://www.americancorpus.org/')
-entry_form = page.form('zabba')
+
+#click "Enter" button
+entry_form = page.form("zabba")
 page = agent.submit(entry_form)
+
+puts "Entered COCA Website"
+
+#log in
+login_form = page.form("myform")
+login_form.email = EMAIL
+login_form.password = PASSWORD
+page = agent.submit(login_form)
+
+puts "Logged in"
+
+pp page
+
+
