f3561da26faa2ff8feb19dafe4f103021bb1928c
arobck <andrew.roback@gmail.com>
2011-08-16 00:47:55 -0500

    example file for config constants

---
+++ config/constants_exammple.rb
@@ -0,0 +1,5 @@
+#config CONSTANTS
+#prevents user info from being stored in the clear
+EMAIL = "$your_email_address"
+PASSWORD = "$your_COCA_password"
+OUTPUT_PATH = "$absolute path minus the file name for data file"
