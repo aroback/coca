f8cc6a3720ebfb9b106e7b137704c6c0d6c75c14
arobck <andrew.roback@gmail.com>
2011-08-27 03:52:20 -0500

    counter still does not work-10p fine, 10+off by -1

--- coca.rb
+++ coca.rb
@@ -34,7 +34,6 @@ begin
       puts "Wrote " + results_counter.to_s() + " pages of results for search term #{i}"
     next
     else
-    results_counter += 1
     #check the checkbox in results frame
     b.frame(:name, "rmid").checkbox(:name => "dox").set
     #click context button
@@ -44,22 +43,25 @@ begin
     #get search results in html
     loop do
       f.puts b.frame(:name, "rbottom").table(:index =>2).html
+      results_counter += 1
       next_page = b.frame(:name, "rbottom").text.include? ">>" 
       if next_page != true then
        puts "Wrote " + results_counter.to_s() + " page of results for search term #{i}"
+#got rid of results_counter = 0 here
        break
       else
         b.frame(:name, "rbottom").link(:index =>3).click
-        results_counter += 1
         sleep 5
         page_next = b.frame(:name, "rbottom").link(:index =>3).href
         page_last = b.frame(:name, "rbottom").link(:index =>4).href
         if  page_next == page_last then
           f.puts b.frame(:name, "rbottom").table(:index =>2).html
+          results_counter += 1
           b.frame(:name, "rbottom").link(:index =>3).click
           sleep 5
           f.puts b.frame(:name, "rbottom").html
 	  puts "Wrote " + results_counter.to_s() + " pages of results for search term #{i}"
+          results_counter = 0
         break
         else
         end#if 3
